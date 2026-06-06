55fb5ecdb359f34465bd271f39db84d0ed96d8d1
Cmunoz <cmunozm6@miumg.edu.gt>
2026-06-06 10:11:32 -0600

    factory para chef agregado

--- database/factories/ChefFactory.php
+++ database/factories/ChefFactory.php
@@ -2,4 +2,22 @@
 
 namespace Database\Factories;
 
-use App\Models\Chef;
+use Illuminate\Database\Eloquent\Factories\Factory;
+/**
+ * @extends Factory<\App\Models\Chef>
+ */
+class ChefFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'specialty' => fake()->word(),
+        ];
+    }
+}
