47313ea60a1d529c80813eb9f6cc622aebefd42e
Cmunoz <cmunozm6@miumg.edu.gt>
2026-06-06 09:48:32 -0600

    creacion de las tablas chefs y recipes desde migrations

---
+++ database/migrations/2026_06_06_154101_create_recipes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('category', 255);
+            $table->integer('preparation_time')->unsigned()->default(0);
+            $table->foreignId('chef_id')->constrained('chefs')->onDelete('cascade');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
